9dc3047c3b9319b648db02d5b3da08f9d2745524
ibrahimkebapci <60468692+ibrahimkebapci@users.noreply.github.com>
2022-05-08 22:19:48 -0700

    First update

---
+++ Mob Fight/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+    public float speed = .1f;
+    void Update()
+    {
+        float xDirection = Input.GetAxisRaw("Horizontal");
+        float zDirection = Input.GetAxisRaw("Vertical");
+        Vector3 moceDirection = new Vector3(xDirection, 0.0f,zDirection).normalized;
+
+        transform.position += moceDirection;
+        
+    }
+}
