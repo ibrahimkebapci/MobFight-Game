0d4c843cb2611b154bbee82355c8dfe9e986bfc8
ibrahimkebapci <60468692+ibrahimkebapci@users.noreply.github.com>
2022-06-01 15:53:41 -0700

    Update

--- Mob Fight/Assets/Scripts/Player.cs
+++ Mob Fight/Assets/Scripts/Player.cs
@@ -5,23 +5,44 @@ using System;
 
 public class Player : MonoBehaviour
 {
+    public Camera camera;
     Vector3 imlecDunya;
     //public float speed = 1f;
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Enemy")
         {
-            Destroy(gameObject);
+           Destroy(gameObject);
         }
+    }
+    private void Start()
+    {
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
     void Update()
     {
-        //GetComponent<Rigidbody>().velocity = Vector3.forward * 5;
-        if(Input.GetMouseButtonDown(0))
+        if(Input.touchCount >0)
         {
-            imlecDunya = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20)); 
+            Touch parmak = Input.GetTouch(0);
+            RaycastHit dokunulanNesne;
+            if(Physics.Raycast(camera.ScreenPointToRay(parmak.position),out dokunulanNesne))
+            {
+                if(dokunulanNesne.collider.gameObject.name=="Player")
+                {
+                    
+                }
+            }
         }
-        transform.position = Vector3.MoveTowards(transform.position,imlecDunya,.2f);
+
+        //dümdüz hareket ettirme 
+        //GetComponent<Rigidbody>().velocity = Vector3.forward * 5;
+        
+        //t?klad???n yere hareket ettirme
+        //if(Input.GetMouseButtonDown(0))
+        //{
+        //    imlecDunya = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20)); 
+        //}
+        //transform.position = Vector3.MoveTowards(transform.position,imlecDunya,.30f);
     }
 
 }
