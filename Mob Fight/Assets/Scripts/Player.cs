145ae9c08f03853a6c6af41dfdf18d3a5a0a7aeb
ibrahimkebapci <60468692+ibrahimkebapci@users.noreply.github.com>
2022-06-11 11:25:13 -0700

    Update

--- Mob Fight/Assets/Scripts/Player.cs
+++ Mob Fight/Assets/Scripts/Player.cs
@@ -11,9 +11,6 @@ public class Player : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private FixedJoystick _joyistick;
     [SerializeField] private float _moveSpeed;
-    //public Joystick joystick;
-    //public float hiz = 5f ;
-    //Rigidbody rg;
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Enemy")
@@ -23,14 +20,12 @@ public class Player : MonoBehaviour
     }
     private void Start()
     {
-        //rg = GetComponent<Rigidbody>();
+        
     }
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector3 (_joyistick.Horizontal * _moveSpeed , _rigidbody.velocity.y , _joyistick.Vertical* _moveSpeed);
-    //    Vector3 kuvvet = new Vector3(joystick.Horizontal*hiz, 0f, joystick.Vertical*hiz);
-    //    rg.AddForce(kuvvet);
+        _rigidbody.velocity = new Vector3(_joyistick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joyistick.Vertical * _moveSpeed);
     }
-
     
+
 }
